371f7883530f2667a2d7cd48f99ad03204ef9d74
Stepty <stephen7ni@gmail.com>
2023-05-24 18:09:46 -0500

    completed 1, 2, 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfTimes) {
+    let newString = "";
+    if (numOfTimes < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < numOfTimes; i++) 
+    {
+        newString = newString + string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = "";
+    for (let i = 0; i < string.length; i++) 
+    {
 
+        newString += string[string.length - i - 1];
+    }
+    return newString;
 };
 
 // Do not edit below this line
