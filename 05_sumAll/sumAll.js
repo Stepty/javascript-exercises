1429bfcc563c3f049c6f8cfbfd4bb81bfffab3a9
Stepty <stephen7ni@gmail.com>
2023-05-24 21:12:46 -0500

    completed 4,5,6,7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    let sumAll = 0;
+    if (startNum < 0) 
+    {
+        return "ERROR";
+    }
+    if (endNum < 0) 
+    {
+        return "ERROR";
+    }
+    if (!Number.isInteger(startNum) ||!Number.isInteger(endNum))
+    {
+    return "ERROR";
+    }
+    if (endNum > startNum) 
+    {
+        for (let i = startNum; i <= endNum; i++) 
+        {
+            sumAll += i;
+        }
+    }
+    else
+    {
+        for (let i = endNum; i <= startNum; i++) 
+        {
+            sumAll += i;
+        }
+    }
+    return sumAll;
 };
 
 // Do not edit below this line
